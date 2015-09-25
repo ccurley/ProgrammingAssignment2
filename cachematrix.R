c2a81b3edef87a32ac560a42cdeebcb8cdd532d6
Christopher Curley <ccurley@faintingincoils.org>
2015-09-25 11:07:55 -0400

    Clean up typos clearer comments
    I removed the function template / blank that I somehow added back into
    the function text. Cleared up some comments.

--- cachematrix.R
+++ cachematrix.R
@@ -63,21 +63,9 @@ cacheSolve <- function(x, ...) {
         # set and pass matrix M out
         x$seti(m)
         m
-}## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+# End of function
 
 # x <- matrix(c(1, 0, 1, 5, -3, 1, 2, 4, 7), nrow = 3)
 #
